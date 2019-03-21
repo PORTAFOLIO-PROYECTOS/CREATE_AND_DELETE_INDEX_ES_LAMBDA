c186da7646997fcb86898f764a543dc81b980fd8
HugoRoca <hugo.rock20@hotmail.com>
2019-03-21 10:25:58 -0500

    add new files

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 
 //import { request } from "request";
 const elasticsearch = require("elasticsearch");
+const config = require("./config");
+const _base = require("./base");
 
 const config = {
-    countries: ['PE'],
-    elasticUrl: 'https://vpc-es-sbsearch-qa-6lqloaf2kfljixcaekbyqxu2aa.us-east-1.es.amazonaws.com',
-    activeCampaignsUrl: 'http://localhost:63185/api/Campania/activas/{pais}/0/3'
+    
 };
 
 const esClient = new elasticsearch.Client({
@@ -25,32 +25,36 @@ const _utils = {
 }
 // Fin Utils
 
-const _funciones = {
-    inicializar: function () {
-        _funciones.getElasticIndices();
-    },
+class Funciones {
+    async createIndices() {
+        try {
+            let params = {
+                "h": ["index"],
+                "format": "json",
+                "index": "producto*"
+            }
 
-    getElasticIndices: function () {
-        let params = {
-            "h": ["index"],
-            "format": "json",
-            "index": "producto*"
-        }
+            let indices = [];
 
-        let indices = [];
-
-        esClient.cat.indices(params, (error, response, status) => {
-            response.forEach(element => {
-                indices.push(element.index);
-                console.log("INDICES AL OBTENER", element.index);
+            return new Promise((resolve, reject) => {
+                esClient.cat.indices(params, (error, response, status) => {
+                    console.log(error);
+                    response.forEach(element => {
+                        indices.push(element.index);
+                    });
+                    resolve(indices);
+                });
             });
-        });
-
-        return indices;
+        } catch (error) {
+            console.error(error);
+        }
     }
 }
 
-_funciones.inicializar();
+async function init() {
+    const _funciones = new Funciones();
+    console.log("entro");
+    console.log(await _funciones.createIndices());
+}
 
-
-
+init();
