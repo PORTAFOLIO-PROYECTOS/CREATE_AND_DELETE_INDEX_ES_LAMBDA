2a93a76079dd671c5a4bcc471b5c3b002d8c5f2d
HugoRoca <hugo.rock20@hotmail.com>
2019-03-21 11:32:57 -0500

    funtion autoexec

--- index.js
+++ index.js
@@ -2,10 +2,11 @@
 
 const _base = require("./app/base");
 
-(() => {
+(async () => {
     const base = new _base();
 
     let indices = base.getElasticIndices();
+    let listar = await indices;
 
-    console.log(await indices);
+    console.log(listar);
 })();
